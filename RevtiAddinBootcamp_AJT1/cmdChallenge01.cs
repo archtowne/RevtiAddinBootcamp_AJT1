12dc148e3447da5c71e9fd73b357c2bfac34249e
AJT <AJT@AJT-XPS15>
2024-11-16 13:34:55 -0500

    Add project files.

---
+++ RevtiAddinBootcamp_AJT1/cmdChallenge01.cs
@@ -0,0 +1,43 @@
+﻿namespace RevtiAddinBootcamp_AJT1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdChallenge01 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Revit application and document variables
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            // Your Module 01 Challenge code goes here
+            int number1 = 250;
+            int elevation1 = 0;
+            int floorHeight1 = 15;
+
+            List<string> list1 = new List<string>();
+
+            list1.add(number1);
+
+
+            return Result.Succeeded;
+        }
+        internal static PushButtonData GetButtonData()
+        {
+            // use this method to define the properties for this command in the Revit ribbon
+            string buttonInternalName = "btnChallenge01";
+            string buttonTitle = "Module\r01";
+
+            Common.ButtonDataClass myButtonData = new Common.ButtonDataClass(
+                buttonInternalName,
+                buttonTitle,
+                MethodBase.GetCurrentMethod().DeclaringType?.FullName,
+                Properties.Resources.Module01,
+                Properties.Resources.Module01,
+                "Module 01 Challenge");
+
+            return myButtonData.Data;
+        }
+    }
+
+}
